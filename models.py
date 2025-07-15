1de54e82b2809a153b5f7ef745bb1509ab69c622
Jason Takahashi <jmtakahashi@gmail.com>
2025-07-14 17:40:59 -0700

    update user model email column to be unique to match our db

--- models.py
+++ models.py
@@ -25,7 +25,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.Text, nullable=False)
-    email = db.Column(db.Text, nullable=False)
+    email = db.Column(db.Text, unique=True, nullable=False)
     password = db.Column(db.Text, nullable=False)
 
     def __repr__(self):
