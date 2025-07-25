433d32be23ffbc0b062b1d6c871738bf4a509d89
Jason Takahashi <jmtakahashi@gmail.com>
2025-07-24 18:12:26 -0700

    update todo model user_id column delete action

--- models.py
+++ models.py
@@ -74,7 +74,8 @@ class Todo(db.Model):
     __tablename__ = "todos"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        'users.id', ondelete="cascade"), nullable=False)
     todo = db.Column(db.Text, nullable=False)
     date_added = db.Column(db.DateTime, nullable=False, default=datetime.now())
     complete = db.Column(db.Boolean, nullable=False, default=False)
